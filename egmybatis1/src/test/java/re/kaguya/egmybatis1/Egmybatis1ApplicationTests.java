302a214f449d68df38787ce2c905118c8cb361f0
Kaguya <rider0@163.com>
2024-05-13 16:39:34 +0800

    完成任务

---
+++ egmybatis1/src/test/java/re/kaguya/egmybatis1/Egmybatis1ApplicationTests.java
@@ -0,0 +1,13 @@
+package re.kaguya.egmybatis1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Egmybatis1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
