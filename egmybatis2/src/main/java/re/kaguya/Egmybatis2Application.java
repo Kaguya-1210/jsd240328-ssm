3b7965ea2c2b7a1170af279dad47b88adf175007
Kaguya <rider0@163.com>
2024-05-14 17:44:53 +0800

    up homework project

---
+++ egmybatis2/src/main/java/re/kaguya/Egmybatis2Application.java
@@ -0,0 +1,13 @@
+package re.kaguya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Egmybatis2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Egmybatis2Application.class, args);
+    }
+
+}
---
+++ egmybatis2/src/test/java/re/kaguya/Egmybatis2ApplicationTests.java
@@ -0,0 +1,13 @@
+package re.kaguya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Egmybatis2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
