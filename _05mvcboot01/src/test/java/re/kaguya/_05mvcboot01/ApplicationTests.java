3af631a49599720f6b4f566188b501ed65e749c2
Kaguya <rider0@163.com>
2024-05-15 10:19:17 +0800

    up spring MVC

---
+++ _05mvcboot01/src/test/java/re/kaguya/_05mvcboot01/ApplicationTests.java
@@ -0,0 +1,13 @@
+package re.kaguya._05mvcboot01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
