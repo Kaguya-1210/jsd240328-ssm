219df1e5e8d9208d93307c2d69c4b80a670a5838
Kaguya <rider0@163.com>
2024-05-23 10:43:59 +0800

    摆烂了

--- _Weibo_Kaguya/src/main/java/re/kaguya/_weibo_kaguya/config/MvcConfig.java
+++ _Weibo_Kaguya/src/main/java/re/kaguya/_weibo_kaguya/config/MvcConfig.java
@@ -4,12 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import re.kaguya._weibo_kaguya.interceptors.MyInterceptors;
+import re.kaguya._weibo_kaguya.interceptors.UserLoginInterceptors;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new MyInterceptors())
+        registry.addInterceptor(new UserLoginInterceptors())
                 .addPathPatterns("/v1/weibo/**")
                 .addPathPatterns("/v1/comment")
                 .excludePathPatterns("v1/comment/selectByWeiboId");
--- _Weibo_Kaguya/src/main/java/re/kaguya/_weibo_kaguya/interceptors/UserLoginInterceptors.java
+++ _Weibo_Kaguya/src/main/java/re/kaguya/_weibo_kaguya/interceptors/UserLoginInterceptors.java
@@ -5,14 +5,20 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+import re.kaguya._weibo_kaguya.pojo.vo.UserVo;
 
 @Slf4j
 public class UserLoginInterceptors implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-
+        UserVo userVo = (UserVo) request.getSession().getAttribute("user");
+        log.debug("1.preHandle: done.");
+        if (userVo != null) {
+            return true;
+        }
+        response.sendRedirect("/login.html");
+        return false;
     }
-
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
         HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
